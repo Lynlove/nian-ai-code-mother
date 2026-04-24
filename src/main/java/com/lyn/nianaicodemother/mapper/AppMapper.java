fdacf006b23a05298856c66704bfba86f855726f
lyn <3228562873@qq.com>
2026-04-24 11:08:35 +0800

    应用模块增删改查

---
+++ src/main/java/com/lyn/nianaicodemother/mapper/AppMapper.java
@@ -0,0 +1,12 @@
+package com.lyn.nianaicodemother.mapper;
+
+import com.lyn.nianaicodemother.model.entity.App;
+import com.mybatisflex.core.BaseMapper;
+
+/**
+ * 应用 映射层。
+ *
+ */
+public interface AppMapper extends BaseMapper<App> {
+
+}
---
+++ src/main/java/com/lyn/nianaicodemother/service/AppService.java
@@ -0,0 +1,41 @@
+package com.lyn.nianaicodemother.service;
+
+import com.lyn.nianaicodemother.model.dto.app.AppQueryRequest;
+import com.lyn.nianaicodemother.model.entity.App;
+import com.lyn.nianaicodemother.model.vo.AppVO;
+import com.mybatisflex.core.query.QueryWrapper;
+import com.mybatisflex.core.service.IService;
+
+import java.util.List;
+
+/**
+ * 应用 服务层。
+ *
+ */
+public interface AppService extends IService<App> {
+
+    /**
+     * 获取应用封装类
+     *
+     * @param app
+     * @return
+     */
+    AppVO getAppVO(App app);
+
+    /**
+     * 获取应用封装类列表
+     *
+     * @param appList
+     * @return
+     */
+    List<AppVO> getAppVOList(List<App> appList);
+
+    /**
+     * 构造应用查询条件
+     *
+     * @param appQueryRequest
+     * @return
+     */
+    QueryWrapper getQueryWrapper(AppQueryRequest appQueryRequest);
+
+}
